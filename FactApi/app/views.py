dbcdb4d83e53f6e1f3924f87bb855ca0ee157528
Raju Aalakunta <157129711+Unique-man@users.noreply.github.com>
2026-02-09 14:04:57 +0530

    Refactor FactCRUD methods for consistency

--- FactApi/app/views.py
+++ FactApi/app/views.py
@@ -7,45 +7,45 @@ from app.serializers import *
 class FactCRUD(APIView):
     def get(self,request,pk=None):
         if pk == None:
-            FTO=Fact.objects.all()                   # fetches all students from DB.
-            FJD=FactMS(FTO,many=True)                #converts them into Python dictionaries.
-            return Response(FJD.data)                   # - DRF’s Response object wraps the serialized data.sends those dictionaries as JSON(API) to the client
+            FTO=Fact.objects.all()                   
+            FJD=FactMS(FTO,many=True)                
+            return Response(FJD.data)                  
         else:
-            FO=Fact.objects.get(id=pk)   #get specific student object
-            FJD=FactMS(FO)     # means you created a serializer object and passed a Student model instance into it.  #- DRF now knows how to translate that model instance into JSON-like data.
-            return Response(FJD.data)  #- .data Property- When you access SJD.data, DRF converts the model instance into a Python dictionary that is ready to be rendered as JSON.
+            FO=Fact.objects.get(id=pk)   
+            FJD=FactMS(FO)    
+            return Response(FJD.data)  
 
 
     def post(self,request):
-        FJDO=request.data #incoming json data
-        PO=FactMS(data=FJDO) #pass the json data to deserializer
-        if PO.is_valid(): #validate the data
-            PO.save() #save the data to db
-            return Response({"msg":"Fact Created"})#return response
+        FJDO=request.data 
+        PO=FactMS(data=FJDO)
+        if PO.is_valid(): 
+            PO.save() 
+            return Response({"msg":"Fact Created"})
         return Response(PO.errors)
     
-    def put(self,request,pk):
-        FJDO=request.data                                #incoming json data
-        FO=Fact.objects.get(id=pk)                    #get object based on id
-        PO=FactMS(FO,data=FJDO)                       #pass object and json data to deserializer
-        if PO.is_valid():                                #validate the data
-            PO.save()                                    #save the data to db
-            return Response({"msg":"Fact Updated"})      #return response
+    def put(self,request,pk):ta
+        FJDO=request.data                                
+        FO=Fact.objects.get(id=pk)                   
+        PO=FactMS(FO,data=FJDO)                      
+        if PO.is_valid():                               
+            PO.save()                                   
+            return Response({"msg":"Fact Updated"})     
         return Response(PO.errors)
 
     def patch(self,request,pk):
-        FJDO=request.data                                   #incoming json data
-        FO=Fact.objects.get(id=pk)                       #get object based on id
-        PO=FactMS(FO,data=FJDO,partial=True)             #pass object and json data to deserializer
-        if PO.is_valid():                                   #validate the data
-            PO.save()                                       #save the data to db
-            return Response({"msg":"Fact Partially Updated"})         #return response
+        FJDO=request.data                                  
+        FO=Fact.objects.get(id=pk)                     
+        PO=FactMS(FO,data=FJDO,partial=True)             
+        if PO.is_valid():                                   
+            PO.save()                                      
+            return Response({"msg":"Fact Partially Updated"})        
         return Response(PO.errors)
 
     def delete(self,request,pk):
-        PO=Fact.objects.get(id=pk)                       #get object based on id
-        PO.delete()                                         #delete the object
-        return Response({"msg":"Fact Deleted"})             #return response
+        PO=Fact.objects.get(id=pk)                       
+        PO.delete()                                        
+        return Response({"msg":"Fact Deleted"})         
 
     
         
